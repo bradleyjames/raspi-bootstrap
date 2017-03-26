7d7c059a33e358162a219559d205d2d4f133f70b
Reynolds <bradleyjames@gmail.com>
2017-03-25 23:13:39 -0500

    Added docker to bootstrap.

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 
 if [[ $(id -u) -ne  0 ]] 
   then echo "Run as root" 
@@ -10,7 +9,6 @@ if [[ -z "${IP}" ]]
   then echo "IP not defined"
   exit 1
 fi
-
 
 # DHCP
 echo "interface wlan0
@@ -24,8 +22,6 @@ sed -i.bak s/Port\ 22/Port\ 9875/g /etc/ssh/sshd_config
 
 # Create deployer user
 adduser --disabled-password deployer
-
-# Add deployer to sudoers
 echo "
 deployer ALL=(ALL) NOPASSWD: ALL
 Defaults:deployer !requiretty
@@ -34,4 +30,9 @@ Defaults:deployer !requiretty
 addgroup backyardlights
 adduser pi backyardlights
 adduser deployer backyardlights
+
+# Docker
+curl -sSL get.docker.com | sh
+systemctl enable docker # set to autostart
+sudo usermod -aG docker pi # add pi user to docker group
 
